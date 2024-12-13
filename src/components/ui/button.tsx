85c474b2cf409c79503e5ecf05e8445962b2dd44
HoangDucBach <hoangbach0985@gmail.com>
2024-12-13 15:59:58 +0700

    feat: add components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,24 +1,47 @@
-import type { ButtonProps as ChakraButtonProps } from "@chakra-ui/react"
+"use client";
+
+import type { ButtonProps as ChakraButtonProps, RecipeVariantProps } from "@chakra-ui/react"
 import {
   AbsoluteCenter,
   Button as ChakraButton,
   Span,
   Spinner,
+  RecipeCompoundVariant,
+  defineRecipe,
+  useRecipe
 } from "@chakra-ui/react"
 import * as React from "react"
+
+export const buttonRecipe = defineRecipe({
+  base: {
+    display: "flex",
+  },
+  variants: {
+    visual: {
+      solid: { bg: "red.500", color: "white" },
+      outline: { borderWidth: "1px", borderColor: "red.200" },
+    },
+  },
+  defaultVariants: {
+    visual: "solid",
+  }
+})
 
 interface ButtonLoadingProps {
   loading?: boolean
   loadingText?: React.ReactNode
 }
 
-export interface ButtonProps extends ChakraButtonProps, ButtonLoadingProps {}
+export interface ButtonProps extends ChakraButtonProps, ButtonLoadingProps, RecipeVariantProps<typeof buttonRecipe> { }
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   function Button(props, ref) {
     const { loading, disabled, loadingText, children, ...rest } = props
+    const recipe = useRecipe({ recipe: buttonRecipe })
+    const styles = recipe({ ...props, loading })
+
     return (
-      <ChakraButton disabled={loading || disabled} ref={ref} {...rest}>
+      <ChakraButton disabled={loading || disabled} css={styles} ref={ref} {...rest}>
         {loading && !loadingText ? (
           <>
             <AbsoluteCenter display="inline-flex">
